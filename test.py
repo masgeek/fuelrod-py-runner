b0aef021fc8daf07db87ef3cb64d699cb292d441
Sammy M <barsamms@gmail.com>
2023-03-14 10:27:08 +0300

    refactor: updated type.py to have multiple entry points

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 import typer
 from os import getenv
 
+app = typer.Typer()
 
+
+@app.command()
 def main(name: str):
+    print(f"Hello {name}")
 
+
+@app.command()
+def mains(name: str):
     print(f"Hello {name}")
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
